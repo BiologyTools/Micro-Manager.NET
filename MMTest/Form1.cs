b9768ff0cc8c165ae523450952a6d421f07e60e9
Erik Repo <wasd123qwerty@protonmail.com>
2024-07-02 12:27:43 +0300

    Objective control.

--- MMTest/Form1.cs
+++ MMTest/Form1.cs
@@ -1,7 +1,7 @@
 extern alias mmc;
 using mmc::org.micromanager.@internal;
+using mmc::mmcorej;
 using System;
-
 namespace MMTest
 {
     public partial class Form1 : Form
@@ -9,6 +9,7 @@ namespace MMTest
         public Form1(string[] args)
         {
             InitializeComponent();
+            MicroManager.Initialize("C:/Program Files/Micro-Manager-2.0/MMConfig_demo.cfg");
             Directory.SetCurrentDirectory("C:/Program Files/Micro-Manager-2.0/");
             java.lang.System.setProperty("force.annotation.index", "true");
             // Set the library path (adjust the path as needed)
@@ -17,6 +18,11 @@ namespace MMTest
             {
                 MMStudio.main(args);
                 MMStudio ms = MMStudio.getInstance();
+                CMMCore core = ms.core();
+                core.setExposure(5);
+                core.setXYPosition(100, 100);
+                string obj = MicroManager.Objectives.GetObjective(1).Values[0];
+                core.setConfig(MicroManager.TurretName, obj);
             }
             catch (Exception e)
             {
